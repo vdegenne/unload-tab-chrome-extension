d80a6febbacbb94334b1058d668677e050ca8a35
Valentin Degenne <vdegenne@gmail.com>
2025-11-02 11:05:55 +0100

    logo

---
+++ scripts/generate-128-icon.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import sharp from 'sharp'
+import pathlib from 'path'
+
+const __dirname = import.meta.dirname
+
+const imageToTransformFilepath = pathlib.join(
+	__dirname,
+	'../assets/img/pwa-512x512.png'
+)
+const outputFilepath = pathlib.join(
+	__dirname,
+	'../assets/img/chrome-store-128x128.png'
+)
+
+await sharp(imageToTransformFilepath).resize(128, 128).toFile(outputFilepath)
+
+console.log('✅ Image resized and saved to', outputFilepath)
